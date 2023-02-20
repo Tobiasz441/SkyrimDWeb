cfdc0288a3ef48d0d1fa77d9e7a972d4fb92f251
Mateusz Tobiasz <tobiaszmateusz@gmail.com>
2023-02-20 16:34:34 +0100

    First version of the Webpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import {RouterLink, RouterModule, RouterOutlet} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    AsideComponent,
+    FooterComponent,
+    PrivacyPolicyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterLink,
+    RouterOutlet,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
